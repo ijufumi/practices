6f83985078a680e49ba1dce9697dbca76deaf9b9
Takafumi Iju <ijufumi@gmail.com>
2019-05-19 10:50:42 +0900

    Add practice files. (initial commit)

---
+++ paypay/src/main/java/jp/ijufumi/practices/paypay/impl/QueueImpl.java
@@ -0,0 +1,21 @@
+package jp.ijufumi.practices.paypay.impl;
+
+import jp.ijufumi.practices.paypay.Queue;
+
+public class QueueImpl<T> implements Queue<T> {
+    public Queue<T> enQueue(T t) {
+        return null;
+    }
+
+    public Queue<T> deQueue() {
+        return null;
+    }
+
+    public T head() {
+        return null;
+    }
+
+    public boolean isEmpty() {
+        return false;
+    }
+}
