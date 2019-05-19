5394765d03cfcdd113b2efd9758eb1c15cf3bdd4
Takafumi Iju <ijufumi@gmail.com>
2019-05-19 10:50:51 +0900

    Add practice files. (initial commit)

---
+++ paypay/src/test/java/jp/ijufumi/practices/paypay/impl/QueueImplTest.java
@@ -0,0 +1,24 @@
+package jp.ijufumi.practices.paypay.impl;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.Test;
+
+class QueueImplTest {
+
+    @Test
+    void enQueue() {
+    }
+
+    @Test
+    void deQueue() {
+    }
+
+    @Test
+    void head() {
+    }
+
+    @Test
+    void isEmpty() {
+    }
+}
